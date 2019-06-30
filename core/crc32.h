44f9e828a9bac2dedab5c14618d81a55fd7b68a7
Nelson Mongare <mogendi.mongare@gmail.com>
2019-06-30 22:21:14 +0300

    crc_check impl

--- core/crc32.h
+++ core/crc32.h
@@ -5,4 +5,17 @@
 #ifndef CANON_CRC32_H
 #define CANON_CRC32_H
 
+
+#include <zconf.h>
+
+/*
+ * Sources:
+ *      http://www.ross.net/crc/download/crc_v3.txt
+ *      https://docs.microsoft.com/en-us/openspecs/office_protocols/ms-abs/06966aa2-70da-4bf9-8448-3355f277cd77
+ * */
+
+u_int32_t crc32_text(char* text, size_t len);
+
+u_int32_t crc32_bin(char* text, size_t len);
+
 #endif //CANON_CRC32_H
